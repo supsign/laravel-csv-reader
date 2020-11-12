a661f5c734894d30753a20e3d20015a2175e45f2
Florian Ratz <florian.ratz@supsign.ch>
2020-11-12 10:09:52 +0100

    service provider + composer json

---
+++ src/LaravelMfRestServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Supsign\LaravelCsvReader;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCsvReaderServiceProvider extends ServiceProvider
+{
+
+    public function boot() 
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
